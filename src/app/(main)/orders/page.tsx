9340c21c30af738175bea0ab5c3beb7c8e2d854f
hieuhuynh <hieuht@teamsolutions.vn>
2025-05-05 11:40:13 +0700

    nextjs

--- src/app/(main)/orders/page.tsx
+++ src/app/(main)/orders/page.tsx
@@ -9,8 +9,10 @@ import {
   Paper,
   Typography,
 } from "@mui/material";
+import { Suspense } from "react";
 import useSWR from "swr";
 import { axiosClient } from "../../../../config/axios";
+import Loading from "./loading";
 import { Order } from "./OrderModel";
 
 function Page() {
@@ -22,23 +24,25 @@ function Page() {
 
   return (
     <Container>
-      <List>
-        {data?.map((order) => (
-          <Paper key={order.id} sx={{ p: 2, mb: 2 }}>
-            <ListItemText
-              primary={<Typography>ORDER ID: #{order.id}</Typography>}
-              secondary={
-                <Typography>
-                  Date: {new Date(order.orderDate).toLocaleString()}
-                </Typography>
-              }
-            />
-            {order?.listOrderDetails.map((orderDetails) => (
-              <OrderItem key={orderDetails.id} orderDetail={orderDetails} />
-            ))}
-          </Paper>
-        ))}
-      </List>
+      <Suspense fallback={<Loading />}>
+        <List>
+          {data?.map((order) => (
+            <Paper key={order.id} sx={{ p: 2, mb: 2 }}>
+              <ListItemText
+                primary={<Typography>ORDER ID: #{order.id}</Typography>}
+                secondary={
+                  <Typography>
+                    Date: {new Date(order.orderDate).toLocaleString()}
+                  </Typography>
+                }
+              />
+              {order?.listOrderDetails.map((orderDetails) => (
+                <OrderItem key={orderDetails.id} orderDetail={orderDetails} />
+              ))}
+            </Paper>
+          ))}
+        </List>
+      </Suspense>
     </Container>
   );
 }
